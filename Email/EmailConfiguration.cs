e2fc30517eedf1e37c7253af275d6551532af613
Ivan Zubak <ivanzubizubak@gmail.com>
2023-12-04 16:48:21 +0100

    Add 2023. preparation. (#1)

--- Email/EmailConfiguration.cs
+++ Email/EmailConfiguration.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 
 namespace TopSecretSanta.Email;
+
 public sealed class EmailConfiguration
 {
     public string From => "secret.santa@company.com";
-    public IEnumerable<string> CarbonCopies => new List<string>();
     public string SmtpServer => "mail-server.com";
+    public string Username => "secret.santa@company.com";
+    public string Password => "sup3r-strong-smtp-Password";
     public int Port => 25;
     public bool UseSsl => false;
+    public IEnumerable<string> CarbonCopies => new List<string>();
 }
